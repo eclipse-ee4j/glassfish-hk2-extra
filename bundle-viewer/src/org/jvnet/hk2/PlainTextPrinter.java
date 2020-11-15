29a8f5dbbab8061e3184faf304c83e78ec517d30
Piotrek Zygielo <piotr@zygielo.pl>
2020-11-15 17:22:03 +0100

    Remove unused type
    Signed-off-by: Piotrek Zygielo <piotr@zygielo.pl>

--- bundle-viewer/src/org/jvnet/hk2/PlainTextPrinter.java
+++ bundle-viewer/src/org/jvnet/hk2/PlainTextPrinter.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -23,7 +24,7 @@ import java.util.Set;
 /**
  * @author Kohsuke Kawaguchi
  */
-public class PlainTextPrinter implements Printer {
+public class PlainTextPrinter {
     private final PrintStream out;
 
     public PlainTextPrinter(PrintStream out) {
