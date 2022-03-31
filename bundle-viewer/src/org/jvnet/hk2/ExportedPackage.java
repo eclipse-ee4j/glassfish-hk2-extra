09958ed012dba77cfbef2ea73efdc9e22fb4e7a7
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-03-31 11:06:34 +0200

    Fix parsing Export-Packages

--- bundle-viewer/src/org/jvnet/hk2/ExportedPackage.java
+++ bundle-viewer/src/org/jvnet/hk2/ExportedPackage.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -48,6 +49,7 @@ public class ExportedPackage extends Named implements Comparable<ExportedPackage
             sc.read(USES);
             uses = Collections.unmodifiableSet(
                 new TreeSet<String>(Arrays.asList(sc.readUntil('\"').split(","))));
+            sc.readUntil(';');
         } else
             uses = Collections.emptySet();
         if(sc.at(VERSION)) {
