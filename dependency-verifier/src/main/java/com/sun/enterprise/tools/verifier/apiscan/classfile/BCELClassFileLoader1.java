9eae518a7e1e6984683f8c859ee866ddf9ddb758
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-04-01 22:04:36 +0200

    Migrate away from c.s..internal package
    https://bugs.openjdk.java.net/browse/JDK-8132256
    JDK8u342: https://bugs.openjdk.java.net/browse/JDK-8283529

--- dependency-verifier/src/main/java/com/sun/enterprise/tools/verifier/apiscan/classfile/BCELClassFileLoader1.java
+++ dependency-verifier/src/main/java/com/sun/enterprise/tools/verifier/apiscan/classfile/BCELClassFileLoader1.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022 Contributors to Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -20,7 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Logger;
 
-import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.apache.bcel.util.ClassPath;
 
 /**
  * Yet another factory for {@link BCELClassFile}. This is not a public class, as
